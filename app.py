0597cfcae6983f9c80bba74a5d4ab285f128c589
bryan kennedy <bkennedy@smm.org>
2013-09-10 18:24:54 -0500

    Adding psuedo code for aquiring a captured video
    The program will need to not only tell the camera to capture, but also
    download the video into a folder where FLask can see it.

--- app.py
+++ app.py
@@ -12,5 +12,40 @@ def base():
 def capture():
     return render_template('capture.html')
 
+
+def capture_drop():
+    """Switch to video and record 1.5 seconds of video """
+    # None of these will work right this sec, b/c I haven't setup the fabric
+    # recipes above as an import.
+    mode_video()
+    capture_start()
+    sleep(1.5)
+    capture_stop()
+    capture_files = get_captures()
+    url = '%s%s' % (CAPTURE_LIST, capture_files[-1])
+    file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    # Nothing too secret here. This should probably be part of a config script
+    # at some point.
+    f = open('/Library/WebServer/Documents/drop_tower/media/latest.mp4', 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print status,
+
+    f.close()
+
 if __name__ == '__main__':
     app.run(debug=True)
