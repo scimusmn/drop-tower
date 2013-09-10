afc35a7c68a40ef3d8e5ed5f82397f5257447f83
bryan kennedy <bkennedy@smm.org>
2013-09-10 17:56:59 -0500

    INitial Flask application framework
    This gives us a playback and a capture page

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def base():
+    return render_template('capture.html')
+
+
+@app.route('/capture')
+def capture():
+    return render_template('capture.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
